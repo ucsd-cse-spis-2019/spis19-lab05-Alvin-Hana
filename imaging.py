9eb8b102e9a76857b3aa44ca3b112787b0164419
Hana Kim <spis19at@its-cseb230-23.ucsd.edu>
2019-08-19 15:00:33 -0700

    we flipped jaz

--- imaging.py
+++ imaging.py
@@ -13,8 +13,6 @@ def greyscale(im):
 
 def binarize(im, thresh, startx, starty, endx, endy):
     (width, height) = im.size
-    print(width)
-    print(height)
     if startx < 0 or starty < 0 or endx > width or endy > height:
         print("Warning: That's illegal and I just called 911, have fun in jail!")
     else:
@@ -26,11 +24,45 @@ def binarize(im, thresh, startx, starty, endx, endy):
                 if grey < thresh:
                     im.putpixel((x, y),(0, 0, 0))
                 else:
-                    im.putpixel((x, y), (255,255,255))
+                    im.putpixel((x, y),(255,255,255))
 
+def mirrorVert(im):
+    #gets the width and height of picture
+    (width, height) = im.size
+    for x in range( width ):
+        for y in range( height//2 ):
+            (red, green, blue) = im.getpixel((x, y))
+            im.putpixel(((x),(height-y-1)),(red, green, blue))
 
+def mirrorHoriz(im):
+    #gets the width and height of picture
+    (width, height) = im.size
+    for x in range( width//2 ):
+        for y in range( height):
+            (red, green, blue) = im.getpixel((x, y))
+            im.putpixel(((width-x-1),(y)),(red, green, blue))
+
+def flipVert(im):
+#gets the width and height of picture
+    (width, height) = im.size
+    for x in range( width ):
+        for y in range( height//2 ):
+            #creates variables for color on upper half pixels
+            (red, green, blue) = im.getpixel((x, y))
+            #creates color variables on lower half pixels
+            (upperred, uppergreen, upperblue) = im.getpixel((x, height-y-1))
+            #swaps colors of the two pixels
+            im.putpixel(((x),(height-y-1)),(red, green, blue))
+            im.putpixel (((x),(y)),(upperred, uppergreen, upperblue))
+
+            
 
 bear = Image.open("bear.png")
-binarize(bear,127, 0, 0, 600, 800)
-bear.show()
-
+jaz = Image.open("jaz.jpg")
+#binarize(bear,127, 0, 0, 600, 800)
+#binarize(jaz,127, 0, 0, 700,950)
+#mirrorHoriz(jaz)
+#bear.show()
+flipVert(jaz)
+jaz.show()
+#jaz.save("coolest jaz.jpg")
