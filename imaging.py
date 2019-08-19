acf273b34e5192ec80c048e7379c80302450728f
Hana Kim <spis19at@its-cseb230-23.ucsd.edu>
2019-08-19 14:01:16 -0700

    binarize picture

--- imaging.py
+++ imaging.py
@@ -10,8 +10,27 @@ def greyscale(im):
             grey = green*.72+red*.21+blue*.07
             im.putpixel((x, y),(int(grey),int(grey), int(grey)))
 
-bear = Image.open("bear.png")
-greyscale(bear)
-bear.show()
 
+def binarize(im, thresh, startx, starty, endx, endy):
+    (width, height) = im.size
+    print(width)
+    print(height)
+    if startx < 0 or starty < 0 or endx > width or endy > height:
+        print("Warning: That's illegal and I just called 911, have fun in jail!")
+    else:
+        for x in range( startx, endx ):
+            for y in range( starty, endy ):
+                (red, green, blue) = im.getpixel((x, y))
+            #sets the pixel as each opposite RGB value
+                grey = green*.72+red*.21+blue*.07
+                if grey < thresh:
+                    im.putpixel((x, y),(0, 0, 0))
+                else:
+                    im.putpixel((x, y), (255,255,255))
+
+
+
+bear = Image.open("bear.png")
+binarize(bear,127, 0, 0, 600, 800)
+bear.show()
 
