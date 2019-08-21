4882a71a7714b97ce3ece2db83829ecbe4db89af
Alvin Wang <spis19bm@its-cseb230-23.ucsd.edu>
2019-08-20 19:35:54 -0700

    Jaz Likes Jazz

--- greenScreenChallenge.py
+++ greenScreenChallenge.py
@@ -3,27 +3,44 @@ def removeGreen(im, bg):
     #gets the width and height of picture
     (width, height) = im.size
     for x in range( width ):
-        for y in range( 100, height ):
+        for y in range( height ):
             (red, green, blue) = im.getpixel((x, y))
             if green<80 or red>90 or blue>160:
-                bg.putpixel((x+205,y+36),(red,green,blue))
+                bg.putpixel((x+90,y+80),(red,green,blue))
                 
 def scale(im):
     (width, height) = im.size
-    copyim = Image.new('RGB', (width//13,height//13))
-    for x in range( width//13 ):
-        for y in range( height//13 ):
-            (red, green, blue) = im.getpixel((x*13, y*13))
+    copyim = Image.new('RGB', (width//8,height//8))
+    for x in range( width//8 ):
+        for y in range( height//8 ):
+            (red, green, blue) = im.getpixel((x*8, y*8))
             copyim.putpixel((x, y), (red, green, blue))
     return copyim
 
 
-faces=Image.open("faces.jpg")
-newFaces=scale(faces)
-shrek=Image.open("shrek.jpg")
-removeGreen(newFaces, shrek)
-shrek.show()
-shrek.save("jaz's family.jpg")
+
+
+face=Image.open("coolerJaz.jpg")
+newFace=scale(face)
+bee=Image.open("bee.jpg")
+#shrek=Image.open("shrek.jpg")
+removeGreen(newFace, bee)
+bee.show()
+bee.save("JazLikesJazz.jpg")
+
+#face=Image.open("coolerJaz.jpg")
+#bee=Image.open("bee.jpg")
+#newFace=scale(face)
+#bee.show()
+#bee=Image.open("bee.jpg")
+#removeGreen(face, bee)
+#bee.show()
+#faces=Image.open("faces.jpg")
+#newFaces=scale(faces)
+#shrek=Image.open("shrek.jpg")
+#removeGreen(newFaces, shrek)
+#shrek.show()
+#shrek.save("jaz's family.jpg")
 #(red, green, blue) = faces.getpixel((0, 2600))
 #print(red)
 #print(green)
